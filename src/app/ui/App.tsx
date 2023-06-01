0bb5856bd4b8db6c964a6311e84195795e91e764
Money Trees <matveyshakur@gmail.com>
2023-06-01 16:27:08 +0500

    Сделал базовую настройку, добавил все пакеты, создал архитектуру по FSD, начинаю работу с компонентами

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./App.module.scss";
+import { Task } from "@/entities";
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <Task />
+      asdad
+    </div>
+  );
+}
+
+// export default App;
